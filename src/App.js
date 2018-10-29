8ea7e5c93b1c214f22344e4d9990897f5f8ebb01
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-10-29 12:01:23 -0400

    cleanup css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div style={{textAlign: 'center'}}>
+        <header style={{
+          backgroundColor: '#282c34', minHeight: '100vh', display: 'flex', flexDirection: 'column',
+          alignItems: 'center', justifyContent: 'center', fontSize: 'calc(10px + 2vmin)', color: 'white'}}
+        >
+          <img src={logo} style={{animation: 'App-logo-spin infinite 20s linear', height: '40vmin'}}  alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a style={{color: '#61dafb'}} href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
             Learn React
           </a>
         </header>
