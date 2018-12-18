3cff0e8614d2723190deca9e4af401fb691c3f14
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-12-17 23:05:27 -0500

    react loadable implementation overall at 43k chunk.js 36.5k

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import Loadable from 'react-loadable';
+import Loading from './Loading';
+
+const LoadableComponent = Loadable({
+  loader: () => import('./HomePage'),
+  loading: Loading,
+});
+
+export default class App extends React.Component {
   render() {
-    return (
-      <div style={{textAlign: 'center'}}>
-        <header style={{
-          backgroundColor: '#282c34', minHeight: '100vh', display: 'flex', flexDirection: 'column',
-          alignItems: 'center', justifyContent: 'center', fontSize: 'calc(10px + 2vmin)', color: 'white'}}
-        >
-          <img src={logo} style={{animation: 'App-logo-spin infinite 20s linear', height: '40vmin'}}  alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a style={{color: '#61dafb'}} href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <LoadableComponent/>;
   }
 }
-
-export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <header style={{
+          backgroundColor: '#282c34', minHeight: '100vh', display: 'flex', flexDirection: 'column',
+          alignItems: 'center', justifyContent: 'center', fontSize: 'calc(10px + 2vmin)', color: 'white'}}
+        >
+          <img src={logo} style={{animation: 'App-logo-spin infinite 20s linear', height: '40vmin'}}  alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a style={{color: '#61dafb'}} href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
