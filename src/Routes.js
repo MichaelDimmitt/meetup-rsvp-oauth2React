c17f3f05c9850af8d03ad385b6b85d1d1372660f
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-12-20 10:51:11 -0500

    demonstrate redux state mapped to props in another component.

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Loadable from 'react-loadable';
 import Loading from './components/Loading';
+import { connect } from 'react-redux'
 
 const TopBar = Loadable({
   loader: () => import('./components/TopBar'),
@@ -30,7 +31,9 @@ const Login = Loadable({
 
 class Routes extends Component {
   render() {
-    const match = this.props.match;
+    const { match, meetups, meetup, route, isFetching } = this.props
+    console.log('show/demonstrate redux info', { match, meetups, meetup, route, isFetching}, 'show/demonstrate redux info' )
+
     return (
       <div style={{ width: '100vw', height: '20px' }}>
         <TopBar history={this.props.history} />
@@ -50,4 +53,15 @@ class Routes extends Component {
   }
 }
 
-export default Routes;
+function mapStateToProps(state) {
+  console.info('hola some state', state,'hola some state')
+  return {
+    meetups: state.meetups,
+    meetup: state.meetup,
+    route: state.route,
+    session: state.session,
+    isFetching: state.isFetching
+  }
+}
+
+export default connect(mapStateToProps)(Routes)
